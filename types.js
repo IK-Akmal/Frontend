ce3510e70a94a231d4facadc6807fb0eb987bbb8
E.N.D <isomadinovakmal1997@gmail.com>
2022-06-25 19:40:43 +0500

    задание на тему Типы данных

---
+++ types.js
@@ -0,0 +1,12 @@
+"use strict"
+// Шаблонные строки
+// важность: 5
+// Что выведет этот скрипт?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?  овтет hello 1
+
+alert( `hello ${"name"}` ); // ? ответ hello name
+
+alert( `hello ${name}` ); // ? ответ hello Ilya
