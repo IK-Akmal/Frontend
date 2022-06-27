4258cfcedcbce531c98f95c2a4fb7125220f8161
E.N.D <isomadinovakmal1997@gmail.com>
2022-06-27 23:59:20 +0500

    задание на тему Декораторы и переадресация вызова, call/apply

---
+++ Task.6.8.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+
+// Сделайте два варианта решения.
+
+// Используя setInterval.
+// Используя рекурсивный setTimeout.
+
+{
+    function printNumbers(from, to) {
+        let current = from;
+
+        setTimeout(function start() {
+            console.log(current);
+            if (current < to) {
+                setTimeout(start, 1000)
+            }
+            current++;
+        }, 1000)
+    }
+    printNumbers(3,5)
+}
+{
+    function printNumbers(from, to) {
+        let current = from;
+
+       const id = setInterval(function start() {
+            console.log(current);
+            if (current === to) {
+                clearInterval(id)
+            }
+            current++;
+        }, 1000)
+    }
+    printNumbers(3,5)
+}
+
+//---------------------------------------------------------------------------------------
+
